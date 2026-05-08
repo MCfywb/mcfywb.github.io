94970da85d95b4b7783291ac59499e616908f43a
MCfywb <mcfywb@163.com>
2026-05-08 14:54:01 +0800

    音乐馆页配置

---
+++ source/music/index.md
@@ -0,0 +1,9 @@
+---
+title: 音乐馆
+date: 2026-05-07 21:44:17
+type: music
+aplayer: true
+top_img: false
+comments: false
+aside: false
+---
--- themes/anzhiyu/source/js/utils.js
+++ themes/anzhiyu/source/js/utils.js
@@ -845,7 +845,7 @@ const anzhiyu = {
       return;
     }
     const urlParams = new URLSearchParams(window.location.search);
-    const userId = "8152976493";
+    const userId = "17952253068";
     const userServer = "netease";
     const anMusicPageMeting = document.getElementById("anMusic-page-meting");
     if (urlParams.get("id") && urlParams.get("server")) {
